3b1f364fc5dc2bfe2ff7a787d22a337a101eee0d
ChinYoung <woodichin@outlook.com>
2022-03-13 23:12:13 +0800

    micro task

--- nodejs/micro-task.js
+++ nodejs/micro-task.js
@@ -1,9 +1,26 @@
+// task, timers
 (function () {
   setTimeout(() => {
     console.log('setTimeout');
-    return Promise.resolve().then(() => {
-      console.log('promise callback');
-      process.nextTick(() => { console.log('nextTick'); })
-    });
+    process.nextTick(() => { console.log('next tick'); })
+    process.nextTick(() => {
+      console.log('next tick');
+      process.nextTick(() => { console.log('next tick'); })
+    })
   }, 0);
+})();
+
+// task, timers
+(function () {
+  setTimeout(() => {
+    console.log('setTimeout');
+    process.nextTick(() => { console.log('next tick'); })
+  }, 0);
+})();
+
+
+(function () {
+  return Promise.resolve().then(() => { console.log('promise callback'); })
 })()
+
+process.nextTick(() => { console.log('next tick'); })

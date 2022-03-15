d749caa88acbf6b1394e93d91e7948a655f56ae9
yang <woodichin@outlook.com>
2022-03-15 23:10:49 +0800

    wip

--- nodejs/examples/setImmediate/setTimeout.js
+++ nodejs/examples/setImmediate/setTimeout.js
@@ -1,17 +1,23 @@
 const { createServer, request } = require('http')
 const server = createServer()
 
-const maxCall = 20
-const trigger = 10
+const maxCall = 10
+const trigger = 4
 let current = 0
+let printLog = true
 server.on('request', (req, res) => {
-  console.log('request incoming', new Date().valueOf());
+  printLog && console.log('request incoming', current);
   if (current < maxCall) {
+    current += 1
     callLocalhost()
+  } else {
+    current = 0
+    printLog = true
   }
   if (current === trigger) {
     setTimeout(() => {
-      console.log('timer');
+      printLog = false
+      console.log('setTimeout');
     }, 0);
   }
   res.write('hello\n')

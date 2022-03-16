e1dbe39c48fd18308479f4315dace098f5080e8a
ChinYoung <woodichin@outlook.com>
2022-03-16 21:12:05 +0800

    wip

--- nodejs/examples/slice/intensiveCalculation.js
+++ nodejs/examples/slice/intensiveCalculation.js
@@ -6,7 +6,7 @@ let flag = false
 server.on('request', (req, res) => {
   console.log('request incoming', new Date().valueOf());
   if (!flag) {
-    heavyCalculation()
+    intensiveCalculation()
     flag = true
   }
   res.write('hello\n')
@@ -15,7 +15,7 @@ server.on('request', (req, res) => {
 
 server.listen(5001, () => { console.log('server started'); })
 
-function heavyCalculation() {
+function intensiveCalculation() {
   const now = new Date().valueOf()
   while ((new Date().valueOf() - now) < 5000) {
     // calculate
